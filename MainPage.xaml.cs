7601fced40054587689a8f50679470812e97be00
ahmed <ahmet.mandil@gmail.com>
2022-09-21 17:16:01 +0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Android.Util;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+            Result.Text = $"Clicked {count} time";
+		else
+            Result.Text = $"Clicked {count} times";
+
+
+        SemanticScreenReader.Announce(Result.Text);
+	}
+
+	private void OnIncremant(object sender, EventArgs e) {
+
+		count--;
+		if (count == 10)
+            Result.Text = $"Clicked {count} time";
+		else
+            Result.Text = $"Clicked {count} time";
+
+		SemanticScreenReader.Announce(Result.Text);
+
+    }
+}
+
